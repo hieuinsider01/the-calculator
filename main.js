df234c873b49cf5b75a7fd7a5f4fa2e60eac98f0
hieuinsider01 <hieuinsider@DESKTOP-FEH88FH.localdomain>
2025-10-22 20:38:19 +0700

    Refactor JS code. Create state to store current state of the calculator and operate function

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
-// State 1
+// Create state object and operate function
 const calculatorState = {
     displayValue: '0',
     firstOperand: null,
     waitingForSecondOperand: false,
     operator: null,
 }
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    if (b === 0) {
+        return "Cannot divide by zero";
+    } else {
+        return a / b;
+    }
+}
+// Create operator object
+const operator = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+};
+// Operate Function
+function operate(operatorSymbol, a, b) {
+    a = Number(a);
+    b = Number(b);
+    const operationFunction = operator[operatorSymbol];
+    if (operationFunction) {
+        return operationFunction(a, b);
+    } else {
+        return null;
+    }
+}
 
-
-// State 2
-// Select all digit button
-const allDigitBtns = document.querySelector('.digit-btn');
-// Loop for all digit button
-allDigitBtns.forEach(button => {
-    button.addEventListenner('click', (event) => {
-        const inputValue = event.target.dataset.value;
-
-        console.log(inputValue)
-    })
-})
+console.log(operate('+', 1, 2))
